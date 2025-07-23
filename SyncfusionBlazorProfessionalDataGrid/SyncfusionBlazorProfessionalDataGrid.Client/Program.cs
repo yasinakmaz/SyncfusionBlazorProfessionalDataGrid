928489398d045d20ea05fa08781997ce31a0d32a
Yasin Akmaz <155752189+yasinakmaz@users.noreply.github.com>
2025-07-23 18:15:48 +0300

    Update Program.cs

--- SyncfusionBlazorProfessionalDataGrid/SyncfusionBlazorProfessionalDataGrid.Client/Program.cs
+++ SyncfusionBlazorProfessionalDataGrid/SyncfusionBlazorProfessionalDataGrid.Client/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SyncfusionBlazorProfessionalDataGrid.Client.Services;
 using Syncfusion.Blazor;
 
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXhcc3RdQ2JeVEN/WERWYEk=");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 // Syncfusion servislerini ekleyin
